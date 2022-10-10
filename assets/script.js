5a582fe08fa5b8a376f5c062fd11b6ce52ccbd87
kennacapri <kennac28@gmail.com>
2022-10-10 15:01:20 -0600

    added timer info to js to countdown after each question

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,35 @@
-var myfunc = setinterval(function() {
-    //
-}, 1000)
 
-var now = new Date().getTime();
-var timeleft = countDownDate - now;
+(function() {
+    var sec = 75;
+    function startTimer(){
+    console.log("timer counts down")
+    var timer = setInterval(function(){
+     sec--;   
+    document.getElementById("secs").innerHTML="00:"+sec;
+    if (sec < 0) {
+    clearInterval(timer);
+    alert("Time is up")
+        }
+    }, 1000);
+}
+document.getElementById("incorrect").addEventListener("click", function() {
+    sec -= 5;
+    document.getElementById("timerDisplay").innerHTML="00:"+sec;
+});
+startTimer();
+})();
 
-var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+// var now = new Date().getTime();
+// var timeleft = countDownDate - now;
+
+// var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
 
-document.getElementById("secs").innerHTML = seconds + "s"
+
+
+// function switchScreen ($newScreen) {
+//     if ($newScreen === $currentWindow) return;
+//     HTMLVideoElement($currentWindow);
+
+// }
